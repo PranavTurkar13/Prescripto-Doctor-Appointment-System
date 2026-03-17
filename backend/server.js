74cb4ac98ba84728e1ebc515140f3cff7e00f109
Pranav Turkar <pranavturkar93@gmail.com>
2026-03-17 21:14:43 +0530

    token added

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use(
         return callback(null, true);
       },
       methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-      allowedHeaders: ["Content-Type", "Authorization"],
+      allowedHeaders: ["Content-Type", "Authorization","token"],
       credentials: true,
     })
   );
